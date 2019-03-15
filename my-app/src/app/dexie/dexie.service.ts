9a69fa0a27df4c687a35dd9873bcf533527ef006
Stefanowicz Małgorzata <stefanowicz.ma@gmail.com>
2019-03-15 15:55:58 +0100

    Wyświetl eventy w kalendarzu za pomocą danych z dixie // Part: preparing service...

--- my-app/src/app/dexie/dexie.service.ts
+++ my-app/src/app/dexie/dexie.service.ts
@@ -1,19 +1,8 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class DexieService {
-
-//   constructor() { }
-// }
-
-
 import Dexie from 'dexie';
 
 export class DexieService extends Dexie {
   constructor() {
-    super('Ng2DexieSample');
+    super('onlinePlannerDB');
     this.version(1).stores({
       todos: '++id',
     });
